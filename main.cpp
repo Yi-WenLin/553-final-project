1bb1256d766824ce8b60ae3a8eedd3344d18d591
Yi-WenLin <166192572+Yi-WenLin@users.noreply.github.com>
2025-04-06 14:42:14 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* Author: YiWen Lin, Syed Adnan, Poornima Kakade
+   Date: 04/05/2025
+   Description: 
+
+*/
+
+#include <Menu.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    Menu menu;
+    menu.mainMenu();
+    
+    return 0;
+}
